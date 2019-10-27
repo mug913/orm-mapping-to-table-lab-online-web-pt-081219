a301b2549885b35953deb4dc9a546ec216e4ae14
Brad Alexander <brad.alexander913@gmail.com>
2019-10-27 19:56:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,12 @@ class Student
         )
         SQL
       DB[:conn].execute
-
     end
+
+    def self.drop_table
+      sql = <<-SQL
+        DROP TABLE student
+        SQL
+      DB[:conn].execute
 
 end
